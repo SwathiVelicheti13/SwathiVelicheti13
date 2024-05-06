c9059f027b6e2e550db7db9c0c1c1ac8289ca071
swathi1316 <swathi.velicheti@gmail.com>
2024-05-06 12:35:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home.js';
 import Projects from './pages/Projects.js';
 import Works from './pages/Works.js';
 import ProjectDetail from './pages/ProjectDetail';
 import '../src/styles/global.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <div className="app-container">
-        <div className="hero">
-          <Navbar />
+        <div className="app-container">
+          <div className = 'hero'>
+            <Navbar />
+          </div>
+          <div className = 'content'>
+              <Routes>
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/works" element={<Works />} />
+                <Route path="/project/:id" element={<ProjectDetail />} />
+              </Routes>
+            </div>  
         </div>
-      <div className="content">
-        <Routes>
-        <Route path="/" Component={Home}></Route>
-        <Route path="/projects" Component={Projects}></Route>
-        <Route path="/works" Component={Works}></Route>
-        <Route path="/project/:id" element={<ProjectDetail />} />
-        </Routes>
-      </div>
-      </div>
       </Router>
     </div>
   );
